57a5cd24f9df82e1889d8248d6c00344b20140cf
Thorley <thorley.eric@gmail.com>
2018-06-20 23:25:41 -0400

    buttons are creating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,47 +7,55 @@ var questionArrayCounter = 0 //count through the array of arrays
 var questionArray = [questionOne, questionTwo] //aray of the questions
 var questionOne =  { question:"Who does Brady play for?", choices: ["Packers", "Patriots", "Rams", "Steelers"], answer:"Patriots" }
 var questionTwo =  { question:"Who does Rodgers play for?", choices: ["Packers", "Patriots", "Rams", "Steelers"], answer:"Packers" }
+console.log(questionOne.question)
 
 
-function displayQuestion() {
+$(document).ready(function(){
 
-    $("#quizQuestions").html(questionArray.question); //show the quiz question
-    
-    for (i=0; i < this.choices.length; i++){  //dynamically create the quiz buttons
-        var buttons = $("<button>")
-        buttons.addClass("data-question", choices[i])
-        buttons.addText(choices[i])
-        $("#answers").append(buttons)
-    }
+    $("#quizQuestions").text("Press button to begin")
+    $("#answers").html("<button> START </button>")
 
-
-    //check if answer is correct
-     function checkAnswer(){
-        buttons.on("click", function(check){
-            if (question === this.answer){
-                correct()
-            }
-            else if ((question != answer) || (counter=0)){ 
-                wrong()
-            }
+ $("#answers").on("click", function(){
+    $("#answers").empty()
+        //show the quiz question
+        $("#quizQuestions").html(questionOne.question); 
         
-        })
-    }
+        //dynamically create the quiz buttons
+        for (i=0; i < questionOne.choices.length; i++){  
+            var buttons = $("<button>")
+            buttons.addClass("data-question", i)
+            buttons.text(i)
+            $("#answers").append(buttons)
+        }
 
-    //function to add points move to the next question and display a correct
-    //with a timer
-    var answerCheck = setTimeout(correct, 5000)
-    function correct(){
-        correct++
-        questionArrayCounter++
-        $("#answers").text("That is correct")
-    }
 
-    function wrong(){
-        incorrect++
-        questionArrayCounter++
-        $("answers").text("That is wrong")
-    }
-}
+        //check if answer is correct
+        function checkAnswer(){
+            buttons.on("click", function(check){
+                if (question === this.answer){
+                    correct()
+                }
+                else if ((question != answer) || (counter=0)){ 
+                    wrong()
+                }
+            
+            })
+        }
 
-document.onload(displayQuestion())
+        //function to add points move to the next question and display a correct
+        //with a timer
+        var answerCheck = setTimeout(correct, 5000)
+        function correct(){
+            correct++
+            questionArrayCounter++
+            $("#answers").text("That is correct")
+        }
+
+        function wrong(){
+            incorrect++
+            questionArrayCounter++
+            $("answers").text("That is wrong")
+        }
+    
+  })
+})
